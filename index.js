2e47b1ad724b17146698b20a1430977e94e67996
shal-in <shalin.a.ahasan@gmail.com>
2023-06-20 18:00:10 +0200

    pushing to firebase db

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+
+const appSettings = {
+    databaseURL: 'https://shalin-shopping-cart-default-rtdb.europe-west1.firebasedatabase.app/'
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, 'shoppingList')
+
 const inputEl = document.getElementById('input-field');
 const addButtonEl = document.getElementById('add-button');
 
 function addFunction() {
     if (inputEl.value !== '') {
+        push(shoppingListInDB, inputEl.value);
         console.log(inputEl.value);
         inputEl.value = '';
+        inputEl.placeholder = 'ice cream'; 
     }
 }
 
@@ -17,3 +30,13 @@ inputEl.addEventListener('keyup', e => {
         addFunction();
     }
 })
+
+function clearInput() {
+    inputEl.placeholder = "";
+}
+
+function resetInput() {
+    if (inputEl.value === '') {
+        inputEl.placeholder = "ice cream";
+    }
+}
