b78edc6f049e27c2a8acca75b59860c258564f2a
shal-in <shalin.a.ahasan@gmail.com>
2023-06-21 17:34:50 +0200

    add elements from and to firebase db, some styling features too

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
     databaseURL: 'https://shalin-shopping-cart-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -11,13 +11,37 @@ const shoppingListInDB = ref(database, 'shoppingList')
 
 const inputEl = document.getElementById('input-field');
 const addButtonEl = document.getElementById('add-button');
+const htmlShoppingList = document.getElementById('shopping-list');
+
+let htmlList = [];
+let firstLoad = true;
+
+onValue(shoppingListInDB, function(snapshot) {
+    const shoppingListObject = snapshot.val();
+    if (shoppingListObject) {
+        let shoppingList = Object.values(shoppingListObject);
+        
+        if (firstLoad == true) {
+            for (let element of shoppingList) {
+                addElementsToHTML(element, htmlShoppingList);
+            }
+            firstLoad = false;
+        }
+    }
+});
+
+function addElementsToHTML(element, htmlShoppingList) {
+    let newItem = document.createElement('li');
+    newItem.textContent = element;
+    htmlShoppingList.appendChild(newItem); 
+}
 
 function addFunction() {
     if (inputEl.value !== '') {
         push(shoppingListInDB, inputEl.value);
-        console.log(inputEl.value);
-        inputEl.value = '';
-        inputEl.placeholder = 'ice cream'; 
+        addElementsToHTML(inputEl.value, htmlShoppingList)
+        inputEl.value = ''; 
+        inputEl.placeholder = 'ice cream';
     }
 }
 
