24130cde189fd2b2548a164eef8b4a889c80c097
shal-in <shalin.a.ahasan@gmail.com>
2023-06-23 12:45:09 +0200

    fully working, minor bugs which need to be fixed. visuals need to be cleaned up

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { getDatabase, ref, push, set, remove, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
     databaseURL: 'https://shalin-shopping-cart-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -7,60 +7,116 @@ const appSettings = {
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-const shoppingListInDB = ref(database, 'shoppingList')
+const shoppingListRef = ref(database, 'shoppingList');
+
+let values;
+let entries;
 
 const inputEl = document.getElementById('input-field');
 const addButtonEl = document.getElementById('add-button');
-const htmlShoppingList = document.getElementById('shopping-list');
+const shoppingListEl = document.getElementById('shopping-list');
 
-let htmlList = [];
 let firstLoad = true;
 
-onValue(shoppingListInDB, function(snapshot) {
-    const shoppingListObject = snapshot.val();
-    if (shoppingListObject) {
-        let shoppingList = Object.values(shoppingListObject);
-        
-        if (firstLoad == true) {
-            for (let element of shoppingList) {
-                addElementsToHTML(element, htmlShoppingList);
+onValue(shoppingListRef, (snapshot) => {
+    if (snapshot.exists()) {
+        entries = Object.entries(snapshot.val());
+        values = Object.values(snapshot.val());
+        if (firstLoad === true) {
+            for (let entry of entries) {
+                let entryID = entry[0];
+                let entryValue = entry[1];
+
+                addToHTML(entryID, entryValue);
             }
-            firstLoad = false;
-        }
+        firstLoad = false
+        } 
+    
+
+
     }
 });
 
-function addElementsToHTML(element, htmlShoppingList) {
+
+
+
+inputEl.addEventListener('keyup', (e) => {
+    if (event.keyCode  === 13) {
+        addButtonFunction()
+    }
+})
+
+addButtonEl.addEventListener('click', () => addButtonFunction())
+
+function addButtonFunction() {
+    let value1 = inputEl.value;
+    let value = value1.toLowerCase();
+    if (value == '') {
+        alert('write something first')
+        clearInput()
+        return;
+    }
+     if (values) {
+        if (values.includes(value)) {
+            alert('already in list')
+            clearInput()
+            return;
+        }
+    }
+
+    let validInput = hasLetter(value);
+    if (validInput === false) {
+        alert('invalid input')
+        clearInput()
+        return;
+    }
+
+    let key = generateUniqueId()
+
+    let shoppingListData = ref(database, `shoppingList/${key}`);
+    set(shoppingListData, value);
+
+    addToHTML(key, value);
+    inputEl.value = ''
+}
+
+function addToHTML(key, value) {
     let newItem = document.createElement('li');
-    newItem.textContent = element;
-    htmlShoppingList.appendChild(newItem); 
+    newItem.setAttribute('id', key);
+    newItem.textContent = value
+    newItem.addEventListener('click', () => removeFromHTML(key));
+
+    shoppingListEl.appendChild(newItem)
 }
 
-function addFunction() {
-    if (inputEl.value !== '') {
-        push(shoppingListInDB, inputEl.value);
-        addElementsToHTML(inputEl.value, htmlShoppingList)
-        inputEl.value = ''; 
-        inputEl.placeholder = 'ice cream';
+function removeFromHTML(key) {
+    let shoppingListData = ref(database, `shoppingList/${key}`);
+    remove(shoppingListData);
+
+    let item = document.getElementById(key);
+    if (item) {
+      let parentElement = item.parentNode;
+      parentElement.removeChild(item);
     }
 }
-
-addButtonEl.addEventListener('click', e => {
-    addFunction();
-})
-
-inputEl.addEventListener('keyup', e => {
-    if (e.keyCode === 13) {
-        addFunction();
-    }
-})
 
 function clearInput() {
-    inputEl.placeholder = "";
+    inputEl.value = '';
 }
 
-function resetInput() {
-    if (inputEl.value === '') {
-        inputEl.placeholder = "ice cream";
-    }
-}
+
+
+
+
+function generateUniqueId() {
+    const timestamp = Date.now().toString(36); // Convert current timestamp to base36 string
+    const randomChars = Math.random().toString(36).substr(2, 6); // Generate random characters
+  
+    const uniqueId = timestamp + randomChars; // Concatenate timestamp and random characters
+    return uniqueId;
+  }
+
+  function hasLetter(value) {
+    // Check if the value contains at least one letter
+    return /[a-zA-Z]/.test(value);
+  }
